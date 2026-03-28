c5464771c28c4e9c2789a7a63be9e094e19de801
haoolii <unnhao@gmail.com>
2026-03-28 18:54:06 +0800

    add track

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './carbon.scss'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
